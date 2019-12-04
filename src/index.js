5cf62f8462195ce3113c7a3a908ff310184f7d67
Eric Rinehart <aer0usa@gmail.com>
2019-12-04 01:46:11 -0700

    Built reward points app, first commit to GitHub.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,102 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { purchases, monthNames, customerNames } from './purchases'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const getPoints = (purchase) => {
+    let points = 0;
+    let doublePoints = purchase > 100 ? (purchase - 100) * 2 : 0;
+    let singlePoints = purchase < 100 ? (purchase > 50 ? purchase - 50 : 0) : 50;
+    points = singlePoints + doublePoints;
+    return points;
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const getMonthNumber = (date) => {
+    return new Date(Date.parse(date)).getMonth();
+};
+
+const getCustomers = (purchases) => {
+    let customerRecords = {};
+    let customers = [];
+    purchases.forEach((aPurchase) => {
+        const monthNumber = getMonthNumber(aPurchase.date);
+        if (!customerRecords.hasOwnProperty(aPurchase.customerId)) {
+            customerRecords[aPurchase.customerId] = {
+                months: {},
+                totalPurchase: 0,
+                totalPoints: 0
+            };
+        }
+        if (!customerRecords[aPurchase.customerId].months.hasOwnProperty(monthNumber)) {
+            customerRecords[aPurchase.customerId].months[monthNumber] = {
+                monthlyPurchase: 0,
+                monthlyPoints: 0
+            }
+        }
+        customerRecords[aPurchase.customerId].months[monthNumber].monthlyPurchase += aPurchase.purchase;
+        customerRecords[aPurchase.customerId].months[monthNumber].monthlyPoints += getPoints(aPurchase.purchase);
+        customerRecords[aPurchase.customerId].totalPurchase += aPurchase.purchase;
+        customerRecords[aPurchase.customerId].totalPoints += getPoints(aPurchase.purchase);
+    });
+    customers = Object.keys(customerRecords).sort();
+    return (
+        <div>
+            { customers.map((customerData) => {
+                let monthIndices = Object.keys(customerRecords[customerData].months);
+                return (
+                    <div key={customerData} className='customer'>
+                        <h3>Customer: { customerNames[customerData] }</h3>
+                        { monthIndices.map((aMonth) => {
+                            return (
+                                <p>
+                                    Purchases for { monthNames[aMonth] }:
+                                    ${ customerRecords[customerData].months[aMonth].monthlyPurchase }
+                                    = { customerRecords[customerData].months[aMonth].monthlyPoints } Points
+                                </p>
+                            );
+                        })}
+                        <p>Total Purchases: ${ customerRecords[customerData].totalPurchase }
+                            <span className='right'>Total Points: { customerRecords[customerData].totalPoints }</span>
+                        </p>
+                    </div>
+                );
+            }) }
+        </div>
+    );
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+          <h1>Purchase Rewards</h1>
+          <h2>Customers&rsquo; Purchases by Month</h2>
+          { getCustomers(this.props.purchases) }
+          <h2>All Purchases</h2>
+          <table>
+              <tr>
+                  <th>Customer</th>
+                  <th>Date</th>
+                  <th>Purchase</th>
+                  <th>Points</th>
+              </tr>
+          {this.props.purchases.map((aPurchase, index) => {
+              return (
+                  <tr key={index}>
+                      <td>{ customerNames[aPurchase.customerId] }</td>
+                      <td>{ aPurchase.date }</td>
+                      <td>{ aPurchase.purchase }</td>
+                      <td>{ getPoints(aPurchase.purchase) }</td>
+                  </tr>
+              );
+          })}
+          </table>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App purchases={purchases} />,
+  document.getElementById('root')
+);
---
+++ src/purchases.js
@@ -0,0 +1,117 @@
+const purchases = [
+    {
+        'customerId': 1,
+        'date': 'August 7, 2019',
+        'purchase': 94
+    },
+    {
+        'customerId': 1,
+        'date': 'August 15, 2019',
+        'purchase': 42
+    },
+    {
+        'customerId': 1,
+        'date': 'August 21, 2019',
+        'purchase': 120
+    },
+    {
+        'customerId': 1,
+        'date': 'September 6, 2019',
+        'purchase': 101
+    },
+    {
+        'customerId': 1,
+        'date': 'September 18, 2019',
+        'purchase': 78
+    },
+    {
+        'customerId': 1,
+        'date': 'October 10, 2019',
+        'purchase': 92
+    },
+    {
+        'customerId': 2,
+        'date': 'August 14, 2019',
+        'purchase': 98
+    },
+    {
+        'customerId': 2,
+        'date': 'September 20, 2019',
+        'purchase': 115
+    },
+    {
+        'customerId': 2,
+        'date': 'October 4, 2019',
+        'purchase': 82
+    },
+    {
+        'customerId': 3,
+        'date': 'August 4, 2019',
+        'purchase': 25
+    },
+    {
+        'customerId': 3,
+        'date': 'August 20, 2019',
+        'purchase': 104
+    },
+    {
+        'customerId': 3,
+        'date': 'August 22, 2019',
+        'purchase': 14
+    },
+    {
+        'customerId': 3,
+        'date': 'October 2, 2019',
+        'purchase': 214
+    },
+    {
+        'customerId': 4,
+        'date': 'October 17, 2019',
+        'purchase': 88
+    },
+    {
+        'customerId': 4,
+        'date': 'October 2, 2019',
+        'purchase': 123
+    },
+    {
+        'customerId': 4,
+        'date': 'September 5, 2019',
+        'purchase': 50
+    },
+    {
+        'customerId': 4,
+        'date': 'August 28, 2019',
+        'purchase': 130
+    },
+    {
+        'customerId': 4,
+        'date': 'August 7, 2019',
+        'purchase': 75
+    }
+];
+
+const customerNames = [
+    "R. Stevens",
+    "M. Brown",
+    "H. Smith",
+    "J. Jones",
+    "T. White"
+];
+
+const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+export { purchases, monthNames, customerNames }
